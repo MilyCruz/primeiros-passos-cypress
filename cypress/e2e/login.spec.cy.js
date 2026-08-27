9e269f5ff250c04b4baa2a2aee98912a408397c5
Mileidy <mileidycruzreyes@gmail.com>
2026-08-27 15:27:48 -0300

    User update scenario added

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -8,16 +8,37 @@ describe('Orange HRM Tests', () => {
     loginButton: "[type='submit']",
     secionTitleTopBar: ".oxd-topbar-header-title",
     dashboardGrid: " .orangehrm-dashboard-grid",
-    wrongCredentialAlert: "[role='alert']"
-  }  
+    wrongCredentialAlert: "[role='alert']",
+    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+    firstNameField: "[name= 'firstName']",
+    lastNameField: "[name= 'lastName']",
+    genericField: ".oxd-input-group",
+    dataField:"[placeholder='yyyy-mm-dd']",
+    dateCloseButton: ".--close",
+    submitButton: "[type= 'submit']"
+  }    
 
-  it('Login - Success', () => {
+  it.only('User Info Update - Success', () => {
     cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
     cy.get(selectorsList.passwordField).type (userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get(selectorsList.dashboardGrid)
+    cy.get(selectorsList.myInfoButton).click()
+    cy.get(selectorsList.firstNameField).clear().type('Fist Name Test')
+    cy.get(selectorsList.lastNameField).clear().type('Last Name Test')    
+    cy.get(selectorsList.genericField).eq(4).clear().type('Employee')
+    cy.get(selectorsList.genericField).eq(5).clear().type('OtherIdTest')
+    cy.get(selectorsList.genericField).eq(6).clear().type('DriversLicenseTest')
+    cy.get(selectorsList.genericField).eq(7).clear().type('2025-03-10')
+    cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.submitButton).eq(0).click()
+    cy.get('body').should('contain', 'Successfully Updated')
+    cy.get('.oxd-toast-close')
+    
+    
+   
   })
   it('Login - Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
